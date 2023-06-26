18f028894dda03a5ccde19a5ff899371cdb0fe35
manujohnson1234 <manujohnson2002@gmail.com>
2023-06-26 23:01:20 +0530

    added game manager

---
+++ Assets/scripts/gamemanager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static gamemanager Instance { get; private set; }
+
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeed { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }    
+    }
+
+    private void OnDestroy()
+    {
+        if( Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Star()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        gameSpeed = initialGameSpeed;
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeedIncrease *= Time.deltaTime;
+    }
+}

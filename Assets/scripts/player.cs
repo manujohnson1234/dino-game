2f1db1d555ea336bb2977320f6459c36483c1c08
manujohnson1234 <manujohnson2002@gmail.com>
2023-06-26 22:37:48 +0530

    dino jump

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 9.81f * 2f;
+    public float jumpSpeed = 8f;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded)
+        {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump"))
+            {
+                direction += Vector3.up * jumpSpeed;
+            }
+
+        }
+        character.Move(direction * Time.deltaTime);
+    }
+}
